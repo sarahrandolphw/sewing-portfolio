15ae8e3bd3a069e6b3acd8453b095e6c260dd708
Sarah Warren <njr4ss@virginia.edu>
2025-03-29 00:28:48 -0400

    escape char for build

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
 export default function AboutPage() {
     return (
       <div>
-        <p>I am a comptuer science student at the University of Virginia's School of Engineering and Applied Science. The repository for this website can be found <a href="https://github.com/sarahrandolphw/sewing-portfolio">here</a></p>
+        <p>I am a comptuer science student at the University of Virginia&#39;s School of Engineering and Applied Science. The repository for this website can be found <a href="https://github.com/sarahrandolphw/sewing-portfolio">here</a></p>
       </div>
     );
   }
