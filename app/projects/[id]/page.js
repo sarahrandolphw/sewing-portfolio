a2b678924cb258afd80d749e1897f09e1a901ef8
Sarah Warren <njr4ss@virginia.edu>
2025-03-28 20:37:44 -0400

    links to other pages working/ fixing styling

--- app/projects/[id]/page.js
+++ app/projects/[id]/page.js
@@ -1,17 +1,21 @@
+'use client';
+import Image from "next/image";
 import { notFound } from "next/navigation";
-import projects from "@/data/projects";
+import { useParams } from "next/navigation"; 
+import projects from "../../../data/projects";
 
-export default function ProjectPage({ params }) {
-  const project = projects.find((p) => p.id === params.id);
-  if (!project) return notFound();
-
+export default function ProjectPage() {
+  const { id } = useParams();
+  const project = projects.find((p) => p.id === id);
+  if (!project) {
+    notFound();
+  }
   return (
-    <div>
-      <h2>{project.title}</h2>
+    <div className="project-detail">
+      <h1>{project.title}</h1>
+      <Image src={project.image} alt={project.title} width={600} height={400} />
       <p>{project.description}</p>
-      {project.images.map((img, index) => (
-        <img key={index} src={img} alt={project.title} style={{ width: "100%" }} />
-      ))}
+      {/* Add any other details you want to include */}
     </div>
   );
 }
