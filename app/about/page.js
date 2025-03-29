b6f610aee3f8ff0baa09720fbef4e938d292e03b
Sarah Warren <njr4ss@virginia.edu>
2025-03-29 00:43:45 -0400

    footer links: github and linkedin

--- app/about/page.js
+++ app/about/page.js
@@ -5,4 +5,3 @@ export default function AboutPage() {
       </div>
     );
   }
-  // testing my pre-push
--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,6 +2,8 @@ export default function Footer() {
     return (
       <footer className="footer">
         <a href="https://www.instagram.com/seamsbysof/" className="fa fa-instagram"></a>
+        <a href="https://www.linkedin.com/in/sarah-warren-b6604721b/" className="fa fa-linkedin-square"></a>
+        <a href="https://www.github.com/sarahrandolphw/" className="fa fa-github"></a>
         <p>Copyright 2025 Sarah Warren</p>
       </footer>
     );
