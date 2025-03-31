7e3d57ac95633242f74443f5139ae7a1ec88696a
Sarah Warren <njr4ss@virginia.edu>
2025-03-31 11:52:55 -0400

    theme changes

--- app/components/ProjectGallery.js
+++ app/components/ProjectGallery.js
@@ -6,9 +6,11 @@ export default function ProjectGallery() {
   return (
     <div className="gallery">
       {projects.map((project) => (
-        <Link key={project.id} href={`/projects/${project.id}`} className="project-card">
-          <Image src={project.image} alt={project.title} width={300} height={300} />
-          <h3>{project.title}</h3>
+        <Link key={project.id} href={`/projects/${project.id}`} legacyBehavior>
+          <a className="project-card">
+            <Image src={project.image} alt={project.title} width={300} height={300} priority/>
+            <h3>{project.title}</h3>
+          </a>
         </Link>
       ))}
     </div>
--- data/projects.js
+++ data/projects.js
@@ -1,5 +1,12 @@
 const projects = [
     {
+      id: "custom-dress",
+      title: "Margaret's Dress",
+      description: "Made as a birthday gift for my friend, custom made to her exact measurements",
+      image: "/images/m_dress.jpg",
+      images: ["/images/m_dress.jpg", "/images/m_dress2.jpg"],
+    },
+    {
       id: "crochet-lace",
       title: "Crochet Lace",
       description: "Hand-crocheted lace piece.",
@@ -12,13 +19,6 @@ const projects = [
       description: "Made as gift for friends, family, and myself",
       image: "/images/s_zipper.jpg",
       images: ['/images/s_zipper.jpg','/images/zipper2.jpg', '/images/zipper3.jpg'],
-    },
-    {
-      id: "custom-dress",
-      title: "Margaret's Dress",
-      description: "Made as a birthday gift for my friend, custom made to her exact measurements",
-      image: "/images/m_dress.jpg",
-      images: ["/images/m_dress.jpg", "/images/m_dress2.jpg"],
     },
     {
       id: "laptop-case",
