a91e2537339e8811208beb0dedf8d3dbee48374b
Sarah Warren <njr4ss@virginia.edu>
2025-04-01 18:46:12 -0400

    data/ typos

--- data/about.js
+++ data/about.js
@@ -1,5 +1,5 @@
 const text = {
-    description: "I am a comptuer science student at the University of Virginia's School of Engineering and Applied Science. The repository for this website can be found",
+    description: "I am a computer science student at the University of Virginia's School of Engineering and Applied Science. The repository for this website can be found",
     apiinfo: "The current theme for this website is determined using data from open-mateo.com's",
 }
 
--- data/projects.js
+++ data/projects.js
@@ -10,7 +10,7 @@ const projects = [
     {
       id: "crochet-lace",
       title: "Crochet Lace",
-      description: "Hand-crocheted lace piece.",
+      description: "Hand-crocheted lace piece",
       image: "/images/lace.jpg",
       image2: "/images/lace2.jpg",
       images: ["/images/lace.jpg", "/images/lace2.jpg"],
@@ -18,7 +18,7 @@ const projects = [
     {
       id: "zippers",
       title: "Accessories",
-      description: "Made as gift for friends, family, and myself",
+      description: "Made as gifts for friends, family, and myself",
       image: "/images/s_zipper.jpg",
       image2: "/images/fishy.jpg",
       images: ['/images/s_zipper.jpg','/images/zipper2.jpg', '/images/zipper3.jpg', "/images/fishy.jpg", "/images/fishy2.jpg"],
